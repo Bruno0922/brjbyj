a970927ec70d9b7649677f0116e28cbbc559b81d
Bruno Souza <bruno.souza.c2014@gmail.com>
2021-09-24 09:35:46 -0300

    amem

---
+++ react/tcc/src/pages/Inicial/styled.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+const ConteinerInicial = styled.div`
+
+
+.cabecalho {
+    display: flex;
+    
+    padding-top: 4px;
+    padding-bottom: -8px;
+    
+}
+
+.cabe {
+    margin-left: 100px;
+    padding-right: 30px;
+    color: white;
+    padding-top: 8px;
+    
+
+}
+
+.filho {
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+   
+}
+
+.barra {
+    padding-left: 15px;
+    
+}
+.Lup-img {
+    margin-left: 10px;
+    padding-top: 8px;
+}
+.cadastro {
+    margin-left: 10px;
+    padding-top: 8px;
+
+}
+
+.Cabecho {
+    margin-left: 10px;
+    margin-top: 8px;
+}
+
+.cabecalho {
+    background-color: black;
+    opacity: 80%;
+}
+
+.Cabecho {
+    color: white;
+}
+
+.carrinho{ 
+    padding-top: 8px;
+    margin-left: -15px;
+    
+}
+.filho1 {
+    display: flex;
+    flex-direction: row;
+    padding-left: 350px;
+}
+`
+
+export {ConteinerInicial} 
